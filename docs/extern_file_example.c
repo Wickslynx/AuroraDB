aa42791c00dd8ddd681c32eaa627e8b5ba0b6828
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-01 11:09:27 +0100

    Added comments.

--- docs/extern_file_example.c
+++ docs/extern_file_example.c
@@ -3,11 +3,17 @@
 
 
 int main(int argc, char** argv) {
-  AuroraDB db;
-
-  db.setLock("123456);
-  db.cmdArgs(argc, argv);
-  db.InteractiveMode();
+  
+  AuroraDB db; //Calls AuroraDB class.
+  
+  db.addElement("admin"); //Sets up an admin element.
+  db.set("admin", "Admin", "pAssw0rd"); //Adds a "Admin" user to the admin tag with the password "pAssw0rd".
+  
+  db.setLock("123456"); //Sets up a lock with a password that needs to be verified before anything else.
+  
+  db.cmdArgs(argc, argv); //Takes in cmd args.
+  
+  db.InteractiveMode(); //Starts an interactive menu.
 
   return 0;
 }
